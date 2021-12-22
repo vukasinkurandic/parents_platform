c98de00d5e9b1cc5d3ba3e3eb505ef5dbd87b10b
Vukasin <vukasinkurandic@gmail.com>
2021-12-22 21:16:59 +0100

    Newsletter in layout
    Add model Newsletter, register in admin. Create NewsletterForm and implement it in views and home page.

--- layout/views.py
+++ layout/views.py
@@ -1,9 +1,29 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from . models import Newsletter
+from . forms import NewsletterForm
 
 
 def home(request):
-    return render(request, 'layout/index.html')
+    form = NewsletterForm()
+    if request.method == "POST":
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Hvala, Uspešno ste poslali email!'))
+        context = {'form': form}
+        return redirect('layout:home')
+    context = {'form': form}
+    return render(request, 'layout/index.html', context)
 
 
 def contact(request):
     return render(request, 'layout/contact.html')
+
+
+# if request.method == "POST":
+#     subscribedUsers = Newsletter()
+#     post_data = request.POST.copy()
+#     email = post_data.get("exampleInputEmail1", None)
+#     subscribedUsers.email = email
+#     subscribedUsers.save()
