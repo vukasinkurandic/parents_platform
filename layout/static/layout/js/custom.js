984fb300f4a8d2c0500734ffd78c3401150f3cda
Vukasin <vukasinkurandic@gmail.com>
2022-01-20 00:19:04 +0100

    Base filters for all babysitters
    Make 4 basic filters for babysitters and render babysitter profile for family

--- layout/static/layout/js/custom.js
+++ layout/static/layout/js/custom.js
@@ -99,4 +99,42 @@ if (window.location.href == host + 'register_babysitter/' ||
   }
 }
 
+// JAVASCRIPT FOR PRICE FILTER
+const price_range_slid = "http://127.0.0.1:8000/";
+
+if (window.location.href == price_range_slid + "connection/all_babysitters") {
+
+  window.onload = function () {
+    slideOne();
+    slideTwo();
+  }
+
+  let sliderOne = document.getElementById("price_range_min");
+  let sliderTwo = document.getElementById("price_range_max");
+  let displayValOne = document.getElementById("range1");
+  let displayValTwo = document.getElementById("range2");
+  let minGap = 0;
+  let sliderTrack = document.querySelector(".slider-track");
+  let sliderMaxValue = document.getElementById("price_range_min").max;
+
+  function slideOne() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+      sliderOne.value = parseInt(sliderTwo.value) - minGap;
+    }
+    displayValOne.textContent = sliderOne.value;
+    fillColor();
+  }
+  function slideTwo() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+      sliderTwo.value = parseInt(sliderOne.value) + minGap;
+    }
+    displayValTwo.textContent = sliderTwo.value;
+    fillColor();
+  }
+  function fillColor() {
+    percent1 = (sliderOne.value / sliderMaxValue) * 100;
+    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #73c4f4 ${percent1}% , #73c4f4 ${percent2}%, #dadae5 ${percent2}%)`;
+  }
+}
 
