6f9bd20140af595dbce9f594c12265e6a37a270c
Vukasin <vukasinkurandic@gmail.com>
2022-03-02 18:35:20 +0100

    Fix babysitter creations bug

--- layout/context_processors.py
+++ layout/context_processors.py
@@ -10,13 +10,16 @@ def number_waiting_connections(request):
         number_waiting_connections_base = 0
     else:
         if request.user.user_type == 2:
-            profil = get_object_or_404(Babysitter, user_id=request.user.id)
-            all_match_connections_queryset = Connection.objects.filter(
-                babysitter_id=profil.id)
-            all_match_connections_list = list(all_match_connections_queryset)
-
-            for one_match in all_match_connections_list:
-                if one_match.is_matched == None:
-                    number_waiting_connections_base += 1
+            try:
+                profil = Babysitter.objects.get(user_id=request.user.id)
+                all_match_connections_queryset = Connection.objects.filter(
+                    babysitter_id=profil.id)
+                all_match_connections_list = list(
+                    all_match_connections_queryset)
+                for one_match in all_match_connections_list:
+                    if one_match.is_matched == None:
+                        number_waiting_connections_base += 1
+            except Babysitter.DoesNotExist:
+                number_waiting_connections_base = 0
 
     return {'number_waiting_connections_base': number_waiting_connections_base}
